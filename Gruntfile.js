bcc7b9068f652b6b506cb05e84439a0a98bd53f5
Jeff Lyon <jeff@albatrossdigital.com>
2017-11-24 21:39:08 -0800

    Reorganizing dist into docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         app: 'app',
-        dist: 'dist',
+        dist: 'docs',
 
         sass: {
             options: {
